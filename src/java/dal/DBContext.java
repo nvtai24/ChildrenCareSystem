5f66353bf43e655d36ccdb84efa7f6268aa645fd
Nvtai <buiblue123@gmail.com>
2025-01-12 22:02:38 +0700

    first commit

---
+++ src/java/dal/DBContext.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Nvtai
+ */
+public class DBContext {
+
+    protected Connection connection;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/world";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+
+    public DBContext() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void setParameters(PreparedStatement ps, Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            ps.setObject(i + 1, params[i]);
+        }
+    }
+
+    public ResultSet executeQuery(String query, Object... params) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(query);
+        setParameters(ps, params);
+        return ps.executeQuery();
+    }
+
+    protected int executeUpdate(String query, Object... params) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(query);
+        setParameters(ps, params);
+        return ps.executeUpdate();
+    }
+
+}
