bda64e55402c20ec7e127523d911b87d69f50f85
dominhanh149 <169168437+dominhanh149@users.noreply.github.com>
2025-01-25 22:01:15 +0700

    Login

--- src/java/dal/UserDAO.java
+++ src/java/dal/UserDAO.java
@@ -4,10 +4,34 @@
  */
 package dal;
 
-/**
- *
- * @author Nvtai
- */
-public class UserDAO extends DBContext {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.auth.User;
 
+public class UserDAO {
+
+    private DBContext dbContext;
+
+    public UserDAO() {
+        dbContext = new DBContext();
+
+    }
+
+    public User get(String username, String password) {
+        User user = null;
+        
+        String sql = "SELECT `username`, `password` FROM `user` WHERE `username` = ? AND `password` = ?";
+        try (ResultSet rs = dbContext.executeQuery(sql, username, password)) {
+            if (rs.next()) {
+                user = new User();
+                user.setUsername(rs.getString("username"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return user;
+    }
 }
